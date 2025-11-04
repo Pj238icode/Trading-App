e12d717770eb5152a88d70f32a591afab41c4cf2
Prasanjeet_Jena <122697153+Pj238icode@users.noreply.github.com>
2025-11-04 15:17:44 +0530

    Delete frontend directory

--- frontend/src/components/custome/SpinnerBackdrop.jsx
+++
@@ -1,9 +0,0 @@
-const SpinnerBackdrop = () => {
-  return (
-    <div className="fixed inset-0 bg-white bg-opacity-80 flex items-center justify-center z-50">
-      <div className="w-16 h-16 border-4 border-t-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
-    </div>
-  );
-};
-
-export default SpinnerBackdrop;
